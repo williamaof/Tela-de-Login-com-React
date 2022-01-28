234efabe11ae9f333e61dab9136bdacde4c06e31
William Augusto <williamaof8@gmail.com>
2022-01-27 21:00:08 -0300

    criação pagina Home

---
+++ src/view/home/index.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import './home.css';
+import { Link } from 'react-router-dom';
+
+function Home(){
+
+    return(
+        <h1>Home</h1>
+    )
+    
+}
+
+export default Home;
